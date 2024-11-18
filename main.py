a71f82334a1c7378e12cb9eaf16e119230ed84b0
Kyutesune <lily@kitsuneland.org>
2024-11-18 12:26:15 -0500

    Started Asteroids Game

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ def main():
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock
+	dt = 0
 	while(True):
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill("black")
+		dt = pygame.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
